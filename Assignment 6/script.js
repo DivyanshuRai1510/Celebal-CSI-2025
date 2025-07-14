4e86a1130725052b278d2643b0a929f6c73b6e1c
Divyanshu Rai <139582147+DivyanshuRai1510@users.noreply.github.com>
2025-07-14 08:12:09 +0530

    Create script.js

---
+++ Assignment 6/script.js
@@ -0,0 +1,13 @@
+function first(arr, n) {
+  if (!Array.isArray(arr)) return [];
+  if (n === undefined) return arr[0];
+  if (n < 0) return [];
+  return arr.slice(0, n);
+}
+
+// Test Data
+console.log(first([7, 9, 0, -2]));        // 7
+console.log(first([], 3));               // []
+console.log(first([7, 9, 0, -2], 3));     // [7, 9, 0]
+console.log(first([7, 9, 0, -2], 6));     // [7, 9, 0, -2]
+console.log(first([7, 9, 0, -2], -3));    // []
